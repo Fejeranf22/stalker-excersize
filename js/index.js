27ac851edbba84395cb61751de4085974ced9d3c
ty fejerang <fejerang.ty@gmail.com>
2017-12-14 18:20:12 -0800

    this is a javascript exercise

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+console.log("Hello Nice to meet you "+ name + " " + lastName);
+console.log("you are " + age + " years old.");
+stop();
+
+var oldAge = prompt("how old are you? ");
+console.log("You are:  " + (oldAge * 365)+ " "+ "days old.");
+stop();
